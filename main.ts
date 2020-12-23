eccb4ef5b7a2ffc53d5d61af2ef495d3f69436f6
Jouza <jan.hrouza@gmail.com>
2020-12-23 18:02:13 +0100

    dva typy kostek

--- main.ts
+++ main.ts
@@ -1,9 +1,71 @@
+function kresliPuntiky (num: number) {
+    if (1 == num) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (2 == num) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (3 == num) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (4 == num) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (5 == num) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+}
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
 })
+input.onButtonPressed(Button.B, function () {
+    typKostky = 1 - typKostky
+    basic.showString("K" + typKostky)
+})
 input.onGesture(Gesture.Shake, function () {
-    basic.showNumber(randint(1, 6))
+    basic.clearScreen()
+    basic.pause(200)
+    hozenoCislo = randint(1, 6)
+    if (0 == typKostky) {
+        basic.showNumber(hozenoCislo)
+    } else {
+        kresliPuntiky(hozenoCislo)
+    }
 })
-basic.forever(function () {
-	
-})
+let hozenoCislo = 0
+let typKostky = 0
+typKostky = 0
